a23be8b0ba7f6922022ea038041f020cee306015
Lee,Chris <clee@shakopee.k12.mn.us>
2017-04-29 20:17:49 -0500

    Initial commit of Remove-O365PSSession.ps1 o Closes all sessions for Office 365 products

---
+++ O365/Remove-O365PSSession.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+    Remove open O365 PSSessions
+.DESCRIPTION
+    Remove open O365 PSSessions
+.EXAMPLE
+    Remove-O365PSSession.ps1
+.NOTES
+    Created by Chris Lee
+    Date April 20, 2017
+.LINK   
+#>
+
+#################################
+## DO NOT EDIT BELOW THIS LINE ##
+#################################
+
+##Close open All O365 product Sessions
+Write-Verbose -Message "Removing all PSSessions"
+Get-PSSession | Remove-PSSession
+Write-Verbose -Message "Disconnecting SharePoint Online"
+$ExitPowershell = Read-Host -Prompt "Disconnect from O365 (Will close current Powershell Window) [Y]/N"
+If ($ExitPowershell -eq "Y" -OR $ExitPowershell -eq $null -OR $ExitPowershell -eq "")
+    {
+        stop-process -Id $PID
+    }
