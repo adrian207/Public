23a83b260094d3af3b35a183854cb750fe228808
Lee,Chris <clee@shakopee.k12.mn.us>
2017-08-01 22:46:31 -0500

    Added additional comments to make easier to follow.

--- O365/Invoke-O365AzureSync.ps1
+++ O365/Invoke-O365AzureSync.ps1
@@ -25,7 +25,8 @@
     Date September 6, 2016
 .LINK  
     GitHub: https://github.com/clee1107/Public/blob/master/O365/Invoke-O365AzureSync.ps1
-    Blogger: 
+    Blogger:http://www.myitresourcebook.com/2017/08/invoke-o365azuresyncps1.html
+ 
 #>
 
 [Cmdletbinding()]
@@ -56,28 +57,36 @@ Param
     Test-Verbose
 ## endregion
 
-Write-Verbose -Message "Opening PSSession to $Server"
-$Session = New-PSSession -ComputerName $Server
-Write-Verbose -Message "Checking for ADSync.psd1 at $ADSyncPath"
-If (Invoke-command -Session $Session -ArgumentList $ADSyncPath -scriptblock { param ($ADSyncPath) Test-Path $ADSyncPath})
-    {
-        Write-Verbose -Message "ADSync Module found"
-        Invoke-Command -Session $Session -ArgumentList $ADSyncPath -scriptblock { param ($ADSyncPath) Import-Module $ADSyncPath}
-        Write-Verbose -Message "ADSync Module Loaded"
-        If ($type -eq "Delta")
-            {
-                Write-Verbose -Message "Executing Change Only (Delta) Sync"
-                Invoke-Command -Session $Session {Start-ADSyncSyncCycle -PolicyType Delta}
-            }
-        Else
-            {
-                Write-Verbose -Message "Executing Full Sync"
-                Invoke-Command -Session $Session {Start-ADSyncSyncCycle -PolicyType Initial}
-            }
-    }
-Else
-    {
-        Verbose-Error "Unable to locate ADSync.psd1 at $ADSyncPath"
-    }
-Write-Verbose -Message "Removing PSSession to $Server"
-Remove-PSSession $Session
+#################################
+## DO NOT EDIT BELOW THIS LINE ##
+#################################
+
+## Connect to remote server
+    Write-Verbose -Message "Opening PSSession to $Server"
+    $Session = New-PSSession -ComputerName $Server
+## Check that ADSync is present at supplied path
+    Write-Verbose -Message "Checking for ADSync.psd1 at $ADSyncPath"
+    If (Invoke-command -Session $Session -ArgumentList $ADSyncPath -scriptblock { param ($ADSyncPath) Test-Path $ADSyncPath})
+        {
+            Write-Verbose -Message "ADSync Module found"
+            Invoke-Command -Session $Session -ArgumentList $ADSyncPath -scriptblock { param ($ADSyncPath) Import-Module $ADSyncPath}
+            Write-Verbose -Message "ADSync Module Loaded"
+            ## Check if request for Delta Sync otherwise (else) execute full
+                If ($type -eq "Delta")
+                    {
+                        Write-Verbose -Message "Executing Change Only (Delta) Sync"
+                        Invoke-Command -Session $Session {Start-ADSyncSyncCycle -PolicyType Delta}
+                    }
+                Else
+                    {
+                        Write-Verbose -Message "Executing Full Sync"
+                        Invoke-Command -Session $Session {Start-ADSyncSyncCycle -PolicyType Initial}
+                    }
+        }
+    Else
+        {
+            Verbose-Error "Unable to locate ADSync.psd1 at $ADSyncPath"
+        }
+## Close and Remove the remote session
+    Write-Verbose -Message "Removing PSSession to $Server"
+    Remove-PSSession $Session
